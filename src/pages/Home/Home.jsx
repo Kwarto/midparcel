5b07aa8610dae57a5626d8b1b2b14cf6980c27b5
Kwarto <dbitsolu@gmail.com>
2023-01-04 22:34:33 +0000

    Added midparcel starting files

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Detail from '../../components/Detail/Detail'
+import Hero from '../../components/Hero/Hero'
+
+const Home = () => {
+  return (
+      <>
+          <Hero />
+          <Detail />
+      </>
+  )
+}
+
+export default Home
